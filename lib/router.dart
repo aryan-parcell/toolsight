500a8bfba08ac52d416ed7c28c999c1a46643941
aryan-parcell <aryan@parcellplus.com>
2025-10-29 12:51:35 -0400

    Move pages into pages folder

--- lib/router.dart
+++ lib/router.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:toolsight/home.dart';
-import 'package:toolsight/login.dart';
+import 'package:toolsight/pages/home.dart';
+import 'package:toolsight/pages/login.dart';
 
 enum AppRoute {
   home(path: '/', name: 'home'),
