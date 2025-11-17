c5e1a1994076ece4a98cd9b677e2990acd088e1a
aryan-parcell <aryan@parcellplus.com>
2025-11-17 15:46:04 -0500

    Update mock data and remove Drawer/Toolbox models

--- lib/mock_data.dart
+++ lib/mock_data.dart
@@ -1,79 +1,150 @@
-import 'package:toolsight/models/drawer.dart';
-import 'package:toolsight/models/toolbox.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-final List<Toolbox> mockToolboxes = [
-  Toolbox(
-    id: '1',
-    name: "ToolBox #1",
-    needsAudit: true,
-    drawers: [
-      Drawer(
-        id: '1',
-        name: "Drawer #1",
-        present: 1,
-        total: 2,
-        toolStatus: {
-          "Wrench 10mm": 0,
-          "High Precision Calipers": 1,
-        },
-      ),
-      Drawer(id: '2', name: "Drawer #2", present: 0, total: 0, toolStatus: {}),
-      Drawer(
-        id: '3',
-        name: "Drawer #3",
-        present: 2,
-        total: 5,
-        toolStatus: {
-          "Wrench 10mm": 0,
-          "High Precision Calipers": 1,
-          "Hammer": 1,
-          "Drill": 0,
-          "Soldering Iron": 0,
-        },
-      ),
+final toolboxes = [
+  {
+    'name': 'ToolBox #1',
+    'organization_id': '1',
+    'drawers': [
+      {'drawerId': 'd0', 'drawerName': 'Drawer #1'},
+      {'drawerId': 'd1', 'drawerName': 'Drawer #2'},
     ],
-  ),
-  Toolbox(
-    id: '2',
-    name: "ToolBox #2",
-    needsAudit: false,
-    drawers: [
-      Drawer(
-        id: '1',
-        name: "Drawer #1",
-        present: 2,
-        total: 2,
-        toolStatus: {
-          "Wrench 15mm": 1,
-          "High Precision Calipers": 1,
-        },
-      ),
+    'tools': [
+      {'toolId': 't0', 'drawerId': 'd0', 'toolName': 'Wrench 10mm'},
+      {'toolId': 't1', 'drawerId': 'd1', 'toolName': 'Hammer'},
     ],
-  ),
-  Toolbox(
-    id: '3',
-    name: "ToolBox #3",
-    needsAudit: false,
-    drawers: [
-      Drawer(
-        id: '1',
-        name: "Drawer #1",
-        present: 0,
-        total: 2,
-        toolStatus: {
-          "Drill": 0,
-          "Soldering Iron": 0,
-        },
-      ),
-      Drawer(id: '2', name: "Drawer #2", present: 0, total: 0, toolStatus: {}),
+    'status': 'available',
+    'currentUserId': null,
+    'currentCheckoutId': null,
+    'lastAuditId': null,
+  },
+  {
+    'name': 'ToolBox #2',
+    'organization_id': '1',
+    'drawers': [
+      {'drawerId': 'd0', 'drawerName': 'Drawer #1'},
     ],
-  ),
-  Toolbox(
-    id: '4',
-    name: "ToolBox #4",
-    needsAudit: true,
-    drawers: [
-      Drawer(id: '1', name: "Drawer #1", present: 0, total: 0, toolStatus: {}),
+    'tools': [
+      {'toolId': 't0', 'drawerId': 'd0', 'toolName': 'High Precision Calipers'},
+      {'toolId': 't1', 'drawerId': 'd0', 'toolName': 'Screwdriver'},
     ],
-  ),
+    'status': 'available',
+    'currentUserId': null,
+    'currentCheckoutId': null,
+    'lastAuditId': null,
+  },
+  {
+    'name': 'ToolBox #3',
+    'organization_id': '1',
+    'drawers': [
+      {'drawerId': 'd0', 'drawerName': 'Drawer #1'},
+      {'drawerId': 'd1', 'drawerName': 'Drawer #2'},
+      {'drawerId': 'd2', 'drawerName': 'Drawer #3'},
+      {'drawerId': 'd3', 'drawerName': 'Drawer #4'},
+    ],
+    'tools': [
+      {'toolId': 't0', 'drawerId': 'd0', 'toolName': 'Wrench 15mm'},
+      {'toolId': 't1', 'drawerId': 'd0', 'toolName': 'Mallet'},
+      {'toolId': 't2', 'drawerId': 'd1', 'toolName': 'Drill'},
+      {'toolId': 't3', 'drawerId': 'd1', 'toolName': 'Hammer'},
+      {'toolId': 't4', 'drawerId': 'd2', 'toolName': 'Soldering Iron'},
+      {'toolId': 't5', 'drawerId': 'd2', 'toolName': 'Wrench 10mm'},
+      {'toolId': 't6', 'drawerId': 'd3', 'toolName': 'Wrench 10mm'},
+      {'toolId': 't7', 'drawerId': 'd3', 'toolName': 'Hammer'},
+    ],
+    'status': 'available',
+    'currentUserId': null,
+    'currentCheckoutId': null,
+    'lastAuditId': null,
+  },
 ];
+
+final audits = [
+  {
+    'checkoutId': null,
+    'startTime': DateTime.now(),
+    'endTime': DateTime.now().add(const Duration(minutes: 1)),
+    'drawerStates': {
+      'd0': {
+        'drawerStatus': 'user-validated',
+        'imageStoragePath': null,
+        'results': {
+          't0': 'present',
+        },
+      },
+      'd1': {
+        'drawerStatus': 'user-validated',
+        'imageStoragePath': null,
+        'results': {
+          't1': 'present',
+        },
+      },
+    },
+  },
+  {
+    'checkoutId': null,
+    'startTime': DateTime.now(),
+    'endTime': DateTime.now().add(const Duration(minutes: 1)),
+    'drawerStates': {
+      'd0': {
+        'drawerStatus': 'user-validated',
+        'imageStoragePath': null,
+        'results': {
+          't0': 'present',
+          't1': 'present',
+        },
+      },
+    },
+  },
+  {
+    'checkoutId': null,
+    'startTime': DateTime.now(),
+    'endTime': DateTime.now().add(const Duration(minutes: 1)),
+    'drawerStates': {
+      'd0': {
+        'drawerStatus': 'user-validated',
+        'imageStoragePath': null,
+        'results': {
+          't0': 'present',
+          't1': 'present',
+        },
+      },
+      'd1': {
+        'drawerStatus': 'user-validated',
+        'imageStoragePath': null,
+        'results': {
+          't2': 'present',
+          't3': 'present',
+        },
+      },
+      'd3': {
+        'drawerStatus': 'user-validated',
+        'imageStoragePath': null,
+        'results': {
+          't4': 'present',
+          't5': 'present',
+        },
+      },
+      'd4': {
+        'drawerStatus': 'user-validated',
+        'imageStoragePath': null,
+        'results': {
+          't6': 'present',
+          't7': 'present',
+        },
+      },
+    },
+  },
+];
+
+void addMockData() async {
+  for (int i = 0; i < toolboxes.length; i++) {
+    final toolbox = toolboxes[i];
+    final audit = audits[i];
+
+    final auditDoc = await FirebaseFirestore.instance.collection('audits').add(audit);
+
+    toolbox['lastAuditId'] = auditDoc.id;
+
+    await FirebaseFirestore.instance.collection('toolboxes').add(toolbox);
+  }
+}
